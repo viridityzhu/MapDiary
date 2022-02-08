bda80b246d7afb401be666976ee58b8c0c75c2d3
zhanglin <zhanglin136@jd.com>
2022-02-08 17:34:26 +0800

    Design on the front page

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,22 @@
 import React, { Component } from "react";
-import L from "leaflet";
-import "../node_modules/leaflet/dist/leaflet.css";
 import "./App.css";
+import Header from "./components/Layout/Header";
+import SideNav from "./components/Layout/SideNav";
+// import Footer from "./components/Layout/Footer";
+import Mapbox from "./components/Mapbox";
+
 // import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 
 export default class App extends Component {
-    componentDidMount() {
-      var mymap = L.map("mapbox").setView([1.353, 103.81], 13);
-      L.tileLayer(
-        "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
-        {
-          attribution:
-            'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-          maxZoom: 18,
-          id: "mapbox/streets-v11",
-          tileSize: 512,
-          zoomOffset: -1,
-          accessToken: "pk.eyJ1IjoiMTMwNjU2MjkwMiIsImEiOiJja3pjMXNxZXIxdWV6MnBueHJleW05dWcxIn0.RqJpm-2ggMdK1lnVUPuEhw",
-        }
-      ).addTo(mymap);
-    }
   render() {
     return (
-      <div id="wrapper">
-        <div id="mapbox"></div>
+      <div className="app-wrapper">
+        <Header />
+        <div className="content">
+          <SideNav />
+          <Mapbox />
+        </div>
+        {/* <Footer /> */}
       </div>
     );
   }
---
+++ src/components/Layout/Footer/index.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { Footer } from 'antd/lib/layout/layout';
+
+export default class FooterMod extends Component {
+  render() {
+    return (<div>
+        <Footer style={{width:'100vw'}}>
+            Footer
+        </Footer>
+        
+    </div>)
+  }
+}
