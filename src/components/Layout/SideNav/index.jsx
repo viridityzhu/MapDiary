e52ba1dea13bd8148e92905fffa750a2d5321b3c
zhanglin136 <zhanglin136@jd.com>
2022-03-13 18:23:44 +0800

    Update UI in Homepage

--- src/components/Layout/SideNav/index.jsx
+++ src/components/Layout/SideNav/index.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { Button } from "antd";
-import styles from './index.module.less'
+import styles from "./index.module.less";
 
 export default class SideNav extends Component {
   render() {
@@ -9,11 +9,13 @@ export default class SideNav extends Component {
         <div className={styles.sider}>
           <h2 className={styles.header}>Create a Diary Here</h2>
           <h3 className={styles.location}>Jurong East</h3>
-          <div className={styles["edit-area"]}></div>
+          <div className={styles["edit-area"]}>
+          </div>
           <div className={styles.post}>
             <Button type="primary">Submit</Button>
             <Button type="primary">Clear</Button>
           </div>
+          <div className={styles.expand}></div>
         </div>
       </div>
     );
